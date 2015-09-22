218193761a4f7040d869a6122568dc91b7c23084
Jonathan <test@123.de>
2015-09-22 19:17:54 +0200

    created scene with running man
    animation looped

---
+++ Assets/Scripts/ControlScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlScript : MonoBehaviour {
+
+	int i = 1;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0)){
+
+			i++;
+			i %= 2;
+			GetComponent<Animator> ().SetFloat ("moveSpeed", i+1);
+
+		}
+
+	}
+}
